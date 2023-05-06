407114df59e2bc80e4bb73b68fb1a36fa6848a76
dewansh07 <dewanshwadhwa@gmail.com>
2023-05-06 20:06:38 +0530

    learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 String getFullName(String firstName, String lastName) {
-  return 'firstName';
+  return '$firstName $lastName';
 }
 
+void printMyName() {}
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print(getFullName('Foo', 'choco'));
+    print(getFullName('choco', 'nub'));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
