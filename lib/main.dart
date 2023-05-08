5d6c870c1913ee8d0361ff00f86eb0676b53e7a2
dewansh07 <dewanshwadhwa@gmail.com>
2023-05-08 23:29:27 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,20 @@ void main() {
   runApp(const MyApp());
 }
 
+void test() {
+  var names = ['foo', 'nub'];
+  names.add('lawda');
+  final length = names.length;
+  print(names);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
