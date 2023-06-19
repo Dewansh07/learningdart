6ea54a88b28a4e2908a689a2ca97f79a2be1a603
dewansh07 <dewanshwadhwa@gmail.com>
2023-06-19 13:19:34 +0530

    day 6 done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,9 +5,16 @@ import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:learningdart/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -23,7 +30,7 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
@@ -33,7 +40,8 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Padding(
-                padding:
+                //padding use karne ke liye phele humne dono ko wrap kiya colomn main then
+                padding: //whole column ko padding appply kar diya
                     const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
                 child: Column(
                   children: [
@@ -42,6 +50,11 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter the UserName",
                         labelText: "UserName",
                       ),
+                      onChanged: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -53,15 +66,55 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(
-                        minimumSize: Size(150, 40),
-                      ),
-                      onPressed: () {
+
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeroute);
                       },
-                      child: Text("Login"),
-                    )
+                      child: AnimatedContainer(
+                        //container ko clickable banane ke liye wrap with inkwell or gesture blah blah.
+                        // inkwell main animation gesture main plain
+                        //using container kyuki elevateed button
+                        //size change nhi hota hai
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 100 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurpleAccent,
+                            // shape: changeButton
+                            //     ? BoxShape.circle     can use this as well but wtv
+                            //     : BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 20)),
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   style: TextButton.styleFrom(
+                    //     minimumSize: Size(150, 40),
+                    //   ),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeroute);
+                    //   },
+                    //   child: Text("Login"),
+                    // )
                   ],
                 ),
               ),
