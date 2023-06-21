40cf0114291583cf5e8a20ec42d6375590dccf48
dewansh07 <dewanshwadhwa@gmail.com>
2023-06-21 19:39:00 +0530

    kuch toh ho raha

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -34,110 +34,119 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.white,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
         child: SingleChildScrollView(
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/images/hey.png',
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Welcome $name",
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.amber),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  //padding use karne ke liye phele humne dono ko wrap kiya colomn main then
-                  padding: //whole column ko padding appply kar diya
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Enter the UserName",
-                          labelText: "UserName",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Username cannot be empty";
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          name = value;
-                          setState(() {});
-                        },
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: "Enter the Password",
-                          labelText: "Password",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Password cannot be empty";
-                          } else if (value.length < 6) {
-                            return "Password length should be atleast 6";
-                          }
-                          ;
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      Material(
-                        //here we have wraped inkwell cuz we need material
-                        color: Colors.deepPurpleAccent,
-                        borderRadius:
-                            BorderRadius.circular(changeButton ? 50 : 20),
-                        child: InkWell(
-                          splashColor: Colors.pink,
-                          onTap: () => moveToHome(context),
-                          child: AnimatedContainer(
-                            //container ko clickable banane ke liye wrap with inkwell or gesture blah blah.
-                            // inkwell main animation gesture main plain
-                            //using container kyuki elevateed button
-                            //size change nhi hota hai
-                            duration: Duration(seconds: 1),
-                            width: changeButton ? 100 : 150,
-                            height: 50,
-                            alignment: Alignment.center,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/hey.png',
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Welcome $name",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    Padding(
+                      //padding use karne ke liye phele humne dono ko wrap kiya colomn main then
+                      padding: //whole column ko padding appply kar diya
+                          const EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 25),
 
-                            child: changeButton
-                                ? Icon(
-                                    Icons.done,
-                                    color: Colors.white,
-                                  )
-                                : Text(
-                                    "Login",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Enter the UserName",
+                              labelText: "UserName",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Username cannot be empty";
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              name = value;
+                              setState(() {});
+                            },
                           ),
-                        ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Enter the Password",
+                              labelText: "Password",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Password cannot be empty";
+                              } else if (value.length < 6) {
+                                return "Password length should be atleast 6";
+                              }
+
+                              return null;
+                            },
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Material(
+                            //here we have wraped inkwell cuz we need material
+                            color: Colors.deepPurpleAccent,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 20),
+                            child: InkWell(
+                              splashColor: Colors.pink,
+                              onTap: () => moveToHome(context),
+                              child: AnimatedContainer(
+                                //container ko clickable banane ke liye wrap with inkwell or gesture blah blah.
+                                // inkwell main animation gesture main plain
+                                //using container kyuki elevateed button
+                                //size change nhi hota hai
+                                duration: Duration(seconds: 1),
+                                width: changeButton ? 100 : 150,
+                                height: 50,
+                                alignment: Alignment.center,
+
+                                child: changeButton
+                                    ? Icon(
+                                        Icons.done,
+                                        color: Colors.white,
+                                      )
+                                    : Text(
+                                        "Login",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
